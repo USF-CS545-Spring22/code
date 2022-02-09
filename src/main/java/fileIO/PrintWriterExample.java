ad07529ce1913475699390b729fae7ace4945675
Olga Karpenko <okarpenko@usfca.edu>
2022-02-09 13:09:27 -0800

    Added BufferedReader and PrintWriter examples

--- src/main/java/fileIO/PrintWriterExample.java
+++ src/main/java/fileIO/PrintWriterExample.java
@@ -9,12 +9,9 @@ public class PrintWriterExample {
     public static void main(String[] args) {
 
         File file = new File ("newFile.txt");
-        PrintWriter printWriter;
-        try {
-            printWriter = new PrintWriter (file);
+        try (PrintWriter printWriter = new PrintWriter (file)) {
             printWriter.println ("hello");
-            if (printWriter != null)
-                printWriter.close ();
+
         } catch (FileNotFoundException e) {
             System.out.println("Could not find the file.");
         }
