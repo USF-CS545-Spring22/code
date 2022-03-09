71f12e230f87c4cff1078d385d71a8d73ae2076d
Olga Karpenko <okarpenko@usfca.edu>
2022-03-09 12:54:32 -0800

    Added an iterator and stack & queue examples.

--- src/main/java/linkedLists/LinkedList.java
+++ src/main/java/linkedLists/LinkedList.java
@@ -12,12 +12,20 @@ public class LinkedList {
 	/** Inserts a new node to the front of the list */
 	public void insertAtFront(int elem) {
 		Node newNode = new Node(elem);
-		if (head != null) {
+		if (head == null) {
+			head = newNode;
+			tail  = newNode;
+		}
+		else {
+			newNode.setNext(head);
+			head = newNode;
+		}
+		/*if (head != null) {
 			newNode.setNext(head);
 		} else {
 			tail = newNode;
 		}
-		head = newNode;
+		head = newNode; */
 	}
 
 	/**
@@ -114,6 +122,70 @@ public class LinkedList {
 		return elem;
 	}
 
+	public void insertElem(int elem, int index) {
+			Node newNode = new Node(elem);
+			Node current = head;
+			if (index == 0 && head != null) {
+				// insert before the head
+				newNode.setNext(head);
+				head = newNode;
+				return;
+			}
+			int count = 0;
+			while (current != null && (count < index  - 1)) {
+				current  = current.next();
+				count++;
+			}
+			//option 1:  I got out because current is null
+			if (current == null) {
+				if (index == 0) {
+					head = newNode;
+					tail = newNode;
+				}
+				else
+					throw new IllegalArgumentException();
+			}
+			else {
+				// We reached a node at index i - 1
+				newNode.setNext(current.next());
+				current.setNext(newNode);
+				if (newNode.next() == null) {
+					tail = newNode;
+				}
+			}
+
+
+
+
+
+	}
+
+	public ListIterator listIterator() {
+		return new MyListIterator(0);
+	}
+
+	public class MyListIterator implements ListIterator {
+		Node current = null;
+
+		MyListIterator(int index) {
+			current = head;
+			int count = 0;
+			while (current != null && count < index) {
+				count++;
+				current = current.next();
+			}
+		}
+
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		public int next() {
+			int elem = current.elem();
+			current = current.next();
+			return elem;
+		}
+	}
 
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
@@ -121,6 +193,14 @@ public class LinkedList {
 		list.insertAtFront(15);
 		list.insertAtFront(34);
 		list.printNodes();
+		list.insertElem(55, 3);
+		list.printNodes();
+
+		ListIterator it = list.listIterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
+		/*list.printNodes();
 		// System.out.println(list.find(1));
 		System.out.println();
 		list.insert(0, 4);
@@ -130,8 +210,9 @@ public class LinkedList {
 		list.insert(3, 88);
 		list.printNodes();
 		list.insert(5, 100);
-		list.printNodes();
+		list.printNodes(); */
 
 	}
+
 
 }
